930aa72a8e9d0bb23d30464a449029e958e7c374
dembA7 <93755653+dembA7@users.noreply.github.com>
2021-11-26 02:17:56 -0600

    Create rectangulo.h
    Agregada clase hija Rectangulo

---
+++ rectangulo.h
@@ -0,0 +1,54 @@
+/*
+ * Proyecto Áreas
+ * Arturo Cristián Díaz López 
+ * A01709522
+ * 12/11/2021
+ */
+
+// Creación de clase Rectangulo y declaración de sus funciones.
+
+#ifndef RECTANGULO_H
+#define RECTANGULO_H
+
+#include <iostream>
+using namespace std;
+#include "figura.h"
+
+// Creacion de la clase Rectangulo
+
+class Rectangulo: public Figura{
+  public:
+  Rectangulo(): largoRec(0.0), anchoRec(0.0){};
+  double CalcAreaRec();
+  double get_largoRec();
+  double get_anchoRec();
+  void setlargoRec(double laRec);
+  void setanchoRec(double anRec);
+
+  private:
+  double largoRec, anchoRec;
+};
+
+// Funciones de la clase Rectangulo.
+
+double Rectangulo::CalcAreaRec(){
+  return largoRec*anchoRec;
+}
+
+double Rectangulo::get_largoRec(){
+  return largoRec;
+}
+
+double Rectangulo::get_anchoRec(){
+  return anchoRec;
+}
+
+void Rectangulo::setlargoRec(double laRec){
+  largoRec = laRec;
+}
+
+void Rectangulo::setanchoRec(double anRec){
+  anchoRec = anRec;
+}
+
+#endif
