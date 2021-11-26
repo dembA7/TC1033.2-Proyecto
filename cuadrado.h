fa73e4b58e0290f031d071142459c789bbf2c8ce
dembA7 <93755653+dembA7@users.noreply.github.com>
2021-11-26 02:17:10 -0600

    Create cuadrado.h
    Agregada clase hija Cuadrado

---
+++ cuadrado.h
@@ -0,0 +1,45 @@
+/*
+ * Proyecto Áreas
+ * Arturo Cristián Díaz López 
+ * A01709522
+ * 12/11/2021
+ */
+
+// Creación de clase Cuadrado y declaración de sus funciones.
+
+#ifndef CUADRADO_H
+#define CUADRADO_H
+
+#include <iostream>
+using namespace std;
+#include "figura.h"
+
+// Creacion de la clase Cuadrado
+
+class Cuadrado: public Figura{
+  public:
+  Cuadrado(): ladoCua(0.0){};
+  Cuadrado(double laCua): ladoCua(laCua){};
+  double CalcAreaCua();
+  double get_ladoCua();
+  void setladoCua(double laCua);
+
+  private:
+  double ladoCua;
+};
+
+// Funciones de la clase Cuadrado.
+
+double Cuadrado::CalcAreaCua(){
+  return ladoCua * ladoCua;
+}
+
+double Cuadrado::get_ladoCua(){
+  return ladoCua;
+}
+
+void Cuadrado::setladoCua(double laCua){
+  ladoCua = laCua;
+}
+
+#endif
