bb058b801827bf38fe8b3c9e2dec2479203bd985
dembA7 <93755653+dembA7@users.noreply.github.com>
2021-11-29 23:21:27 -0600

    Update figura.h

--- figura.h
+++ figura.h
@@ -11,6 +11,7 @@
 #define FIGURA_H
 
 #include <iostream>
+#include "perimetro.h"
 using namespace std;
 
 // Creacion de la clase Figura.
@@ -19,6 +20,9 @@ using namespace std;
 class Figura{
   public:
   Figura(): numLados(0), numVertices(0){};
+  Perimetro PCua;
+  Perimetro PRec;
+  Perimetro PCirc;
   int get_numLados();
   int get_numVertices();
   void setnumLados(int numLad);
@@ -46,5 +50,30 @@ void Figura::setnumLados(int numLad){
 void Figura::setnumVertices(int numVert){
   numVertices = numVert;
 }
+
+
+int perimetro(){
+  
+  // Para Cuadrado
+
+  Perimetro PCua;
+  float periCua;
+  periCua = PCua.CalcPeriCua(5);
+
+  // Para Rectangulo
+
+  Perimetro PRec;
+  float periRec;
+  periRec = PRec.CalcPeriRec(10, 4);
+
+  // Para Circulo
+
+  Perimetro PCirc;
+  float periCirc;
+  periCirc = PCirc.CalcPeriCirc(6);
+
+return 0;
+}
+
 
 #endif
