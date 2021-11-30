a49a8c6f9518273a321856479ef9101b5f37bef7
dembA7 <93755653+dembA7@users.noreply.github.com>
2021-11-29 23:11:12 -0600

    Update main.cpp
    Eliminados acentos para evitar errores al imprimir en consola

--- main.cpp
+++ main.cpp
@@ -26,11 +26,11 @@ int main() {
   cuadrado1.setnumLados(4);
   cuadrado1.setnumVertices(4);
   areaCua = cuadrado1.CalcAreaCua();
-  cout << " El área del cuadrado es " << areaCua;
+  cout << " El area del cuadrado es " << areaCua;
   cout << " si el lado vale " << cuadrado1.get_ladoCua() << endl;
-  cout << " además, la figura tiene " << cuadrado1.get_numLados();
+  cout << " ademas, la figura tiene " << cuadrado1.get_numLados();
   cout << " lados, y " << cuadrado1.get_numVertices();
-  cout << " vértices." << endl;
+  cout << " vertices." << endl;
   
   cout << " " << endl;
 
@@ -44,12 +44,12 @@ int main() {
   rectangulo2.setnumLados(4);
   rectangulo2.setnumVertices(4);
   areaRec = rectangulo2.CalcAreaRec();
-  cout  << " El área del rectángulo es " << areaRec;
+  cout  << " El area del rectangulo es " << areaRec;
   cout  << " si el largo vale " << rectangulo2.get_largoRec() << endl; 
   cout  << " y el ancho vale " << rectangulo2.get_anchoRec();
-  cout  << " además, la figura tiene " << cuadrado1.get_numLados();
+  cout  << " ademas, la figura tiene " << cuadrado1.get_numLados();
   cout  << " lados, y " << cuadrado1.get_numVertices();
-  cout  << " vértices." << endl;
+  cout  << " vertices." << endl;
 
   cout << " " << endl;
   
@@ -62,11 +62,11 @@ int main() {
   circulo3.setnumLados(0);
   circulo3.setnumVertices(0);
   areaCirc = circulo3.CalcAreaCirc();
-  cout << " El área del círculo es " << areaCirc << " ";
+  cout << " El area del círculo es " << areaCirc << " ";
   cout << " si el radio vale " << circulo3.get_radioCirc() << endl;
-  cout << " además, la figura tiene " << cuadrado1.get_numLados();
+  cout << " ademas, la figura tiene " << cuadrado1.get_numLados();
   cout << " lados, y " << cuadrado1.get_numVertices();
-  cout << " vértices." << endl;
+  cout << " vertices." << endl;
   
   return 0;
 } 
