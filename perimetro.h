b7fdb11ebd1c8bbb0592a53d19d2ca2991cb11f1
dembA7 <93755653+dembA7@users.noreply.github.com>
2021-12-02 23:42:48 -0600

    Update perimetro.h

--- perimetro.h
+++ perimetro.h
@@ -7,42 +7,69 @@
 
 // Creacion de clase Perimetro y declaracion de funciones.
 
-#include <iostream>
-using namespace std;
+#ifndef PERIMETRO_H
+#define PERIMETRO_H
+
+// Creacion de la clase Perimetro.
 
 class Perimetro{
   public:
-  Perimetro():lado_Cua(0.0), largo_Rec(0.0), ancho_Rec(0.0), radio_Circ(0.0){};
-  float CalcPeriCua(double );
-  float CalcPeriRec(double, double );
-  float CalcPeriCirc(double );
-  void setlado_Cua(double la_Cua);
-  void setlargo_Rec(double lar_Rec);
-  void setancho_Rec(double an_Rec);
-  void setradio_Circ(double rad_Circ);
+    Perimetro():lado_Cua(0.0), largo_Rec(0.0), ancho_Rec(0.0), radio_Circ(0.0){};
+    Perimetro(double ladoC, double largoR, double anchoR, double radioC): lado_Cua(ladoC),
+                                                                          largo_Rec(largoR),
+                                                                          ancho_Rec(anchoR),
+                                                                          radio_Circ(radioC){};
+    double get_ladoCua();
+    double get_largoRec();
+    double get_anchoRec();
+    double get_radioCirc();    
+  
+    void setlado_Cua(double la_Cua);
+    void setlargo_Rec(double lar_Rec);
+    void setancho_Rec(double an_Rec);
+    void setradio_Circ(double rad_Circ);
+  
+    double CalcPeriCua(double );
+    double CalcPeriRec(double, double );
+    double CalcPeriCirc(double );
 
   private:
-  double lado_Cua;
-  double largo_Rec; 
-  double ancho_Rec; 
-  double radio_Circ;
-  float periCua;
-  float periRec;
-  float periCirc;
-
+    double lado_Cua;
+    double largo_Rec; 
+    double ancho_Rec; 
+    double radio_Circ;
+    float periCua;
+    float periRec;
+    float periCirc;
 };
 
 // Funciones de la clase Perimetro.
 
-float CalcPeriCua(double lado_Cua){
-  return 4*lado_Cua;
+double Perimetro::get_ladoCua(){
+  return lado_Cua;
 }
 
-float CalcPeriRec(double largo_Rec, double ancho_Rec){
+double Perimetro::get_largoRec(){
+  return largo_Rec;
+};
+
+double Perimetro::get_anchoRec() {
+  return ancho_Rec;;
+}
+
+double Perimetro::get_radioCirc() {
+  return radio_Circ;
+}
+
+double Perimetro::CalcPeriCua(double lado_Cua){
+  return 4 * lado_Cua;
+}
+
+double Perimetro::CalcPeriRec(double largo_Rec, double ancho_Rec){
   return 2*largo_Rec + 2*ancho_Rec;
 }
 
-float CalcPeriCirc(double radio_Circ){
+double Perimetro::CalcPeriCirc(double radio_Circ){
   return 2*3.14*radio_Circ;
 }
 
@@ -61,3 +88,5 @@ void Perimetro::setancho_Rec(double an_Rec){
 void Perimetro::setradio_Circ(double rad_Circ){
   radio_Circ = rad_Circ;
 }
+
+#endif
